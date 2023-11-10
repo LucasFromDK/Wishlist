a4cd888d9b5feb26cc29e4dd2d1415e9fb8b6bb2
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-11-10 14:49:20 +0100

    Minor Changes

--- snowHandler.js
+++ snowHandler.js
@@ -1,7 +1,7 @@
 function createSnow() {
     const snowHolder = document.getElementById("snowHolder");
 
-    for (let i = 0; i < 150; i++) {
+    for (let i = 0; i < 200; i++) {
         let snowElement = document.createElement("div");
         snowElement.className = "snowfall";
         snowHolder.appendChild(snowElement);
